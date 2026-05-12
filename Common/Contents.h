b4d8d6ca7e58056b71e4fb05578919f6b16a49cc
RAT <70635536+oufguy@users.noreply.github.com>
2026-05-12 21:23:55 +0300

    Commit Test

--- Common/Contents.h
+++ Common/Contents.h
@@ -272,7 +272,4 @@ sprintf(szTemp, "SiegeCooldown in float : %f", _RAT_SiegeCooldown);
 g_pD3dApp->m_pChat->CreateChatChild(szTemp, COLOR_SKILL_USE, CHAT_TAB_SYSTEM);
 #endif
 
-#define TEST_Git
-
-
 
